98ea06ff451d5a54e21087185df21c4edb8787b8
thadtastic <leothaddeus92@gmail.com>
2024-03-03 18:20:49 -0600

    testing build

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Component, useState } from 'react'
 import './App.css'
-import Hello from './components/hello'
+import Hello from '/components/hello';
 
 function App() {
 
   return (
     <>
       <div>
-        <Hello/>
+        <Hello />
       </div>
       
     </>
